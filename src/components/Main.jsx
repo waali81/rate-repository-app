fc9fee0177dd578da343e7edceb70eacdd3a413e
Timo Waali <waali.timo@gmail.com>
2026-09-04 11:06:32 +0300

    10.3

---
+++ src/components/Main.jsx
@@ -0,0 +1,20 @@
+import Constants from 'expo-constants';
+import { StyleSheet, View } from 'react-native';
+import RepositoryList from './RepositoryList';
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: Constants.statusBarHeight,
+    flex: 1,
+  },
+});
+
+const Main = () => {
+  return (
+    <View style={styles.container}>
+      <RepositoryList />
+    </View>
+  );
+};
+
+export default Main;
