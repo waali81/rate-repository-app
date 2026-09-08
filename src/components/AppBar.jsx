07978c6828a5d5c60294d86fb98f814e724d8367
Timo Waali <waali.timo@gmail.com>
2026-09-08 08:08:36 +0300

    10.7

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,4 +1,4 @@
-import { View, StyleSheet, } from 'react-native';
+import { View, StyleSheet, ScrollView } from 'react-native';
 import Constants from 'expo-constants';
 import theme from '../theme';
 import AppBarTab from './AppBarTab';
@@ -8,12 +8,17 @@ const styles = StyleSheet.create({
     paddingTop: Constants.statusBarHeight,
     backgroundColor: theme.colors.textPrimary,
   },
+  scrollView: {
+    flexDirection: 'row',
+  },
 });
 
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <AppBarTab />
+      <ScrollView horizontal>
+        <AppBarTab />
+      </ScrollView>
     </View>
   )
 };
