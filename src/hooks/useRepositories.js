c5bd33cea9a6e53cbdb6a53f4493f0eae02c2cdb
Timo Waali <waali.timo@gmail.com>
2026-09-10 09:32:44 +0300

    10.11

--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -1,24 +1,14 @@
-import { useState, useEffect } from 'react';
+import { useQuery } from '@apollo/client/react';
+import { GET_REPOSITORIES } from '../graphql/queries';
 
 const useRepositories = () => {
-  const [repositories, setRepositories] = useState();
-  const [loading, setLoading] = useState(false);
+  const { data, error, loading } = useQuery(GET_REPOSITORIES, {
+    fetchPolicy: 'cache-and-network',
+  });
 
-  const fetchRepositories = async () => {
-    setLoading(true);
+  const repositories = data?.repositories;
 
-    const response = await fetch('http://10.172.91.29:5000/api/repositories');
-    const json = await response.json();
-
-    setLoading(false);
-    setRepositories(json);
-  };
-
-  useEffect(() => {
-    fetchRepositories();
-  }, []);
-
-  return { repositories, loading, refetch: fetchRepositories };
+  return { repositories, loading, error };
 };
 
 export default useRepositories;
