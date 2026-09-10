50147e1e10c2d5a0e5278d46aba23fc5980a8b62
Timo Waali <waali.timo@gmail.com>
2026-09-10 10:14:52 +0300

    10.12

--- src/utils/apolloClient.js
+++ src/utils/apolloClient.js
@@ -1,7 +1,7 @@
 import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
 
 const httpLink = new HttpLink({
-  uri: 'http://10.172.91.29:4000/graphql',
+  uri: process.env.EXPO_PUBLIC_APOLLO_URI,
 });
 
 const createApolloClient = () => {
